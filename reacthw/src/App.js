7a1bb2876b1c7a5c56b48156a6761a1dcbe05b43
Nicole Portalatin <nportalatin@xpanxion.com>
2023-05-23 19:20:23 -0400

    React Homework

---
+++ reacthw/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Helloworld } from './Helloworld';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Helloworld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reacthw/src/Helloworld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './helloworld.css'
+
+
+export const Helloworld = () => {
+  return (
+    <div className='Helloworld'>Hello World</div>
+  )
+}
